d14e700c385013761e11e166060acde69bb0def9
Michele Oliveira <shellyguns@gmail.com>
2024-09-15 19:56:22 -0300

    check logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,8 +5,6 @@ import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
-import boardgame.Board;
-import boardgame.Position;
 import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
